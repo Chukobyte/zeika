dd225d0aeb27fd017af58f8f03c717e894f2f403
Chukobyte <michaelsmith8293@gmail.com>
2024-05-12 16:59:13 -0400

    Adding font header to seika c includes.

--- src/zeika/seika_includes.zig
+++ src/zeika/seika_includes.zig
@@ -1,6 +1,7 @@
 pub const seika = @cImport({
     @cInclude("seika/seika.h");
     @cInclude("seika/rendering/texture.h");
+    @cInclude("seika/rendering/font.h");
     @cInclude("seika/rendering/renderer.h");
     @cInclude("seika/input/input.h");
 });
